c5d0b2f246df92d43202c85035c438fd4e5bb9fe
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-05-26 17:07:43 +0200

    edit markdown

--- js/script.js
+++ js/script.js
@@ -19,3 +19,10 @@ const userAge = parseInt(prompt ("Inserisco l'età dell'utente", 28));
 
 console.log('KM da percorrere', userKm, typeof userKm);
 console.log('Età utente', userAge, typeof userAge);
+
+
+/*   Sulla base di queste informazioni dovrai calcolare il prezzo totale del viaggio, secondo queste regole:
+
+     - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+     - va applicato uno sconto del 20% per i minorenni
+     -  va applicato uno sconto del 40% per gli over 65.*/
