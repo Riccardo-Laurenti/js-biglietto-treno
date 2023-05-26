98089a46e3022c1ddc4be0420667419911ab1dc7
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-05-26 17:02:51 +0200

    I ask the user for the km and age

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-/* Stampo a schermo*/
+/*   Stampo a schermo*/
 console.log('JS OK!');
 
 
+/*   Recupero elemento dal DOM */
+
+const recElement = document.getElementById('target-train');
+
+
+/*   Il programma dovrà chiedere all'utente :
+     - il numero di chilometri che vuole percorrere 
+     - e l'età del passeggero.  */
+
+
+const userKm = parseInt(prompt("Inserisco i km da percorrere", 20));
+
+const userAge = parseInt(prompt ("Inserisco l'età dell'utente", 28));
+
+
+console.log('KM da percorrere', userKm, typeof userKm);
+console.log('Età utente', userAge, typeof userAge);
--- steps.md
+++ steps.md
@@ -1,1 +1,3 @@
+## 1 Recupero Placeholder dal DOM
 
+## 2 Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
